a6d7ca057cb8fe40dc3192f1eecc3ea31d9004d1
João Roberto Galvão de Oliveira <joaorobertogalvao.2008@gmail.com>
2024-12-03 14:19:18 -0300

    Correção do Sort e adicionando um contador de movimentos

--- algorithms-master/algorithms-master/src/main/game/Game.java
+++ algorithms-master/algorithms-master/src/main/game/Game.java
@@ -3,6 +3,9 @@ package main.game;
 import main.game.map.GameMap;
 import main.game.map.Point;
 import main.game.map.TreasureChest;
+
+import main.strategies.Sort;
+import main.strategies.ShortestDistance;
 import main.strategies.FewerObstacles;
 import main.strategies.Voting;
 
@@ -11,29 +14,38 @@ public class Game {
 	private Player player;
 	public Game() {
 		this.gameMap = new GameMap(8, 8);
-		this.player = new Player(new Voting());
+		this.player = new Player(new Sort());
 	}
-	
+
 	public void run() {
+		int num_moves = 0;
+		int[] chestCounter = {0, 0};
+
 		this.gameMap.print();
 		System.out.println();
+
 		while(true) {
 			Point nextPoint = this.player.evaluatePossbileNextStep(gameMap);
 			if (nextPoint == null) {
 				break;
 			} else {
 				String space = this.gameMap.get(nextPoint);
+
 				if (space != null && space.equals(TreasureChest.CHARACTER)) {
 					this.gameMap.openTreasureChest(nextPoint);
+
 					this.gameMap.print();
+					System.out.println();
 					break;
 				} else {
 					this.gameMap.moveRobot(nextPoint);
+					num_moves++;
+
 				}
 			}
 			this.gameMap.print();
-			System.out.println();
 		}
+			System.out.println(num_moves + " jogadas");
 	}
 
 }
--- algorithms-master/algorithms-master/src/main/strategies/Sort.java
+++ algorithms-master/algorithms-master/src/main/strategies/Sort.java
@@ -1,33 +1,12 @@
-//package main.strategies;
-//
-//import java.util.List;
-//import java.util.Random;
-//
-//import main.game.Player;
-//import main.game.map.Map;
-//import main.game.map.Point;
-//import main.game.map.TreasureChest;
-//
-//public class Sort implements Strategy{
-//	/**
-//	 * N is the next location
-//	 * p1 p2 p3
-//	 * p4 N p5
-//	 * p6 p7 p8
-//	 */
-//	@Override
-//	public Point evaluatePossbileNextStep(List<Point> possibleNextSteps, Map map) {
-//		return null;
-//	}
-//}
-
 package main.strategies;
 
 import java.util.List;
 import java.util.Random;
 
 import main.game.map.GameMap;
+import main.game.map.Monster;
 import main.game.map.Point;
+import main.game.map.Rock;
 
 public class Sort implements Strategy {
 
@@ -39,11 +18,22 @@ public class Sort implements Strategy {
 
 	@Override
 	public Point evaluatePossbileNextStep(List<Point> possibleNextSteps, GameMap gameMap) {
-		if (possibleNextSteps.isEmpty()) {
+		if (possibleNextSteps == null || possibleNextSteps.isEmpty()) {
 			return null;
 		}
-		int index = random.nextInt(possibleNextSteps.size());
-		return possibleNextSteps.get(index);
+
+		Point nextStep = null;
+
+		while (nextStep == null) {
+			int index = random.nextInt(possibleNextSteps.size());
+			nextStep = possibleNextSteps.get(index);
+
+			String space = gameMap.get(nextStep);
+			if (Rock.CHARACTER.equals(space) || Monster.CHARACTER.equals(space)) {
+				nextStep = null;
+			}
+		}
+
+		return nextStep;
 	}
 }
-
